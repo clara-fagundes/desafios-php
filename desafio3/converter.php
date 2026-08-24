ad5db9240cad54fb9c07eff49978588a3ff8dec2
Clara Fagundes <cfagundes236@gmail.com>
2026-08-24 16:19:15 -0300

    adição do desafio 3

---
+++ desafio3/converter.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversão Monetária</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <main>
+    <h1>Conversão Monetária</h1>
+    <?php 
+        $Real = $_REQUEST['valor'];
+        $valorConvertido = $Real / 5.15;
+
+        echo "<h2>O valor R$ " . $Real . " equivalem a US$ " . number_format($valorConvertido, 2, ',', '.') . "</h2>";
+    ?>
+    <p><strong>Cotação fixa de R$5,15</strong> informada diretamente no código</p>
+    <button onclick="window.location.href='index.html'">Voltar</button>
+    </main>
+</body>
+</html>
