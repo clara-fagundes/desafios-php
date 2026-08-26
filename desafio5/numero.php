dd99da54f51b358393bfb1b9839662951af718b3
Clara Fagundes <cfagundes236@gmail.com>
2026-08-26 14:29:12 -0300

    ultimas alterações

---
+++ desafio5/numero.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <main>
+        <h1>Analisador de Numero Real</h1>
+            <?php
+                $num = $_POST['num'] ?? 0;
+
+                echo "<p>Analisando o numero <strong>" . number_format($num, 3, ',', '.') . " </strong> informado pelo usuário</p>";
+
+                $int = (int)$num;
+                $fra = $num - $int;
+
+                echo "<ul><li>A parte inteira do numero é <strong>" . number_format($int, 0 , ",", ".") . "</strong></li>";
+                echo "<li>A parte decimal do numero é <strong>" . number_format($fra, 3 , ",", ".") . "</strong></li></ul>";
+
+                ?>
+            <button onclick="javascript:history.go(-1)"> Voltar </button>
+    </main>
+</body>
+</html>
